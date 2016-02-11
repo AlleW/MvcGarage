3cf44ce0b18dc3d83c179bc8b48c778003e7afa2
AlleW <Alle299792>
2016-02-11 15:22:53 +0100

    Test

---
+++ MvcGarageGroup/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using MvcGarage.CommonFunctions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcGarageGroup.Models
+{
+        [Table("Vehicle")]
+        public class Vehicle
+        {
+            [Key]
+            public int VehicleID { get; set; }
+            public Enumerators.VehicleType VehicleTypeID { get; set; }
+            public string Color { get; set; }
+            public string LicencePlate { get; set; }
+            // Test
+        }
+   
+}
