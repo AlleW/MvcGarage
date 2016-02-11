38af8ce7db32ebc4b0f4e8e148c314739e1d223b
AlleW <Alle299792>
2016-02-11 13:43:09 +0100

    Testing

---
+++ MvcGarageGroup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcGarageGroup.Startup))]
+namespace MvcGarageGroup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
