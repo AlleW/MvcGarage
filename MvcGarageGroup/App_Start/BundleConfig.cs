4beef780b1605a60fa6c1e0bd278f7c677f75092
disajohansdotter <disa.johansdotter@gmail.com>
2016-02-29 10:59:03 +0100

    Disa added angular in BundleConfig

--- MvcGarageGroup/App_Start/BundleConfig.cs
+++ MvcGarageGroup/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MvcGarageGroup
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular*"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css", "~/Content/Garage.css"));
